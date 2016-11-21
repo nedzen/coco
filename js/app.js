866f1a72e82cf33b061d451b7fe42a61835958f7
nedzen <nedzen.11@gmail.com>
2016-11-22 00:25:42 +0100

    carusel works as intended

--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
-$('#myModal').on('shown.bs.modal', function () {
-	$('#myInput').focus()
-})
+$(document).ready(function(){
+	var owl = $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        items:1,
+        nav:true
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1100:{
+        items:3,
+        nav:true,
+        loop:false
+      }
+    }
+
+	});
+	// Go to the next item
+	$('.NextBtn').click(function() {
+	  owl.trigger('next.owl.carousel');
+	})
+	// Go to the previous item
+	$('.PrevBtn').click(function() {
+	  // With optional speed parameter
+	  // Parameters has to be in square bracket '[]'
+	  owl.trigger('prev.owl.carousel', [300]);
+	})
+});
+
+// var owl = $('.owl-carousel');
+
+// owl.owlCarousel();
+// // Go to the next item
+// $('.NextBtn').click(function() {
+//     owl.trigger('next.owl.carousel');
+// })
+// // Go to the previous item
+// $('.PrevBtn').click(function() {
+//     // With optional speed parameter
+//     // Parameters has to be in square bracket '[]'
+//     owl.trigger('prev.owl.carousel', [300]);
+// })
